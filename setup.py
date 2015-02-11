fedc522b9a6a418948db96cf8a53976c11b38c85
Ilya Shalyapin <ishalyapin@gmail.com>
2015-02-12 02:43:44 +0500

    0.3.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name     = 'django-resized',
-    version  = '0.3.0',
+    version  = '0.3.1',
     packages = ['django_resized'],
     requires = ['python (>= 2.7)', 'django (>= 1.7)'],
     description  = 'Resizes image origin to specified size.',
